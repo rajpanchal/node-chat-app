756077a9c71b8189327f0536ee2bd6467b04d4c6
rajpanchal <hemantkumar.2017@vitstudent.ac.in>
2018-07-15 23:04:30 +0530

    add new message and create message events

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+
+var socket = io();
+
+socket.on('connect', function() {
+  console.log('Connected to the server');
+
+  socket.emit('createMessage', {
+    from: 'raj',
+    text: 'Im in'
+  });
+});
+
+socket.on('disconnect', function() {
+  console.log('Disconnected from the server');
+});
+socket.on('newMessage', function(message) {
+  console.log('newMessage', message);
+});
--- server/server.js
+++ server/server.js
@@ -6,28 +6,38 @@ const socketIO = require('socket.io');          //To install web sockets dead ea
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
-var server = http.createServer(app);            //CONFIGURING HTTP TO WORK ON SOCKET-----2
+var server = http.createServer(app);           //CONFIGURING HTTP TO WORK ON SOCKET-----2
 var io = socketIO(server);
 
 
-app.use(express.static(publicPath));            //LOADS INDEX.HTML FROM PUBLIC DIRECTORY
+app.use(express.static(publicPath));           //LOADS INDEX.HTML FROM PUBLIC DIRECTORY
 
 io.on('connection', (socket) => {              //io.on Lets u register an event listener, 'connection' - Listen for a new connection
     console.log('New user connected');
+
+    socket.emit('newMessage', {                  //newEmail 1
+      from: 'rajpanchal243@gmail.com',
+      text: 'Hey dude whats up',
+      createAt: 123
+    });
+
+    socket.on('createMessage', (message) => {           //createEmail 2
+      console.log("createMessage", message);
+    });
 
     socket.on('disconnect', () => {
       console.log('User was disconnected');
     });
 });
 
-server.listen(port, () => {                     //Server.listen instead of app.listen bcz of var server = http.createServer(app);
+server.listen(port, () => {                    //Server.listen instead of app.listen bcz of var server = http.createServer(app);
   console.log(`Server started on ${port}`);
 });
 
 
 /*
   IMP:-
-    Client fires "Connected to the server"
-    Server fires "New user connected"
+    Client fires "Connected to the server" when connected
+    Server fires "New user connected" when connected
     THIS MEANS CONNECTION BTW CLIENT & SERVER IS SUCCESSFULLY ESTABLISHED
 */
