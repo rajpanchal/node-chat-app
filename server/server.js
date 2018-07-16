45130c3e73bf2d8113c9cef512c14f1bdef3cd3d
rajpanchal <hemantkumar.2017@vitstudent.ac.in>
2018-07-16 19:52:09 +0530

    add function instead

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');                   //CONFIGURING HTTP TO WORK ON SOCKET----------1
 const express = require('express');
 const socketIO = require('socket.io');          //To install web sockets dead easily
+const {generateMessage} = require('./utils/message');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -16,25 +17,13 @@ app.use(express.static(publicPath));           //LOADS INDEX.HTML FROM PUBLIC DI
 io.on('connection', (socket) => {              //io.on Lets u register an event listener, 'connection' - Listen for a new connection
           console.log('New user connected');
 
-          socket.emit('newMessage', {
-            from: 'Admin',
-            text: 'Welcome to the chat room',
-            createdAt: new Date().getTime()
-          });
+          socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app!'));
 
-          socket.broadcast.emit('newMessage', {
-            from: 'Admin',
-            text: 'New user connected to the chat room',
-            createdAt: new Date().getTime()
-          });
+          socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user connected!'));
 
           socket.on('createMessage', (message) => {           //createEmail 2
             console.log("createMessage", message);
-            io.emit('newMessage', {
-              from: message.from,
-              text: message.text,
-              createAt: new Date().getTime()
-            });
+            io.emit('newMessage',generateMessage(message.from, message.text));
             // socket.broadcast.emit('newMessage', {
             //   from: message.from,
             //   text: message.text,
